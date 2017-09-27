db5c9ee4c37777697b606794f6c4c5b7b49fc4ab
creecros <creecros@gmail.com>
2017-09-26 23:16:17 -0400

    Add files via upload

---
+++ Action/AutoCreateSubtask.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Kanboard\Plugin\AutomaticAction\Action;
+
+use Kanboard\Model\TaskModel;
+use Kanboard\Action\Base;
+
+class AutoCreateSubtask extends Base
+{
+
+    public function getDescription()
+    {
+        return t('Create a Subtask Automatically');
+    }
+
+    public function getCompatibleEvents()
+    {
+
+        return array(
+	    TaskModel::EVENT_CREATE_UPDATE,
+            TaskModel::EVENT_MOVE_COLUMN,
+        );
+    }
+
+    public function getActionRequiredParameters()
+    {
+        return array(
+            'column_id' => t('Column'),
+            'user_id' => t('Assignee'),
+            'title' => t('SubTitle'),
+        );
+    }
+
+    public function getEventRequiredParameters()
+    {
+        return array(
+            'task_id',
+	    'task' => array(
+                'project_id',
+                'column_id',
+            ),
+        );
+    }
+
+    public function doAction(array $data)
+    {
+	 $values = array(
+            'title' => $this->getParam('title'),
+            'task_id' => $data['task_id'],
+            'user_id' => $this->getParam('user_id'),
+            'time_estimated' => $this->getParam('time_estimated'),
+            'time_spent' => 0,
+            'status' => 0,
+        );
+       return $this->subtaskModel->create($values);
+    }
+
+    public function hasRequiredCondition(array $data)
+    {
+        return $data['task']['column_id'] == $this->getParam('column_id');
+    }
+}
---
+++ Plugin.php
@@ -0,0 +1,52 @@
+<?php
+
+
+
+namespace Kanboard\Plugin\AutomaticAction;
+
+
+
+use Kanboard\Core\Plugin\Base;
+
+use Kanboard\Plugin\AutomaticAction\Action\AutoCreateSubtask;
+
+
+
+class Plugin extends Base
+
+{
+    
+	public function initialize()
+    
+	{
+        
+		$this->actionManager->register(new AutoCreateSubtask($this->container));
+    
+	}
+
+	
+	public function getPluginName()	
+	{ 		 
+		return 'Auto Subtasks'; 
+	}
+
+	public function getPluginAuthor() 
+	{ 	 
+		return 'Craig Crosby'; 
+	}
+
+	public function getPluginVersion() 
+	{ 	 
+		return '0.0.1'; 
+	}
+
+	public function getPluginDescription() 
+	{ 
+		return 'Adding automatic actions for subtasks'; 
+	}
+
+	public function getPluginHomepage() 
+	{ 	 
+		return 'https://github.com/creecros/AutoSubtasks'; 
+	}
+}
