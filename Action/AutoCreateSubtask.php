6cefc51f50104c60c344021cc7f3554c4d3e8128
creecros <creecros@gmail.com>
2017-10-30 23:19:49 -0400

    Update AutoCreateSubtask.php
    adding feature request #2

--- Action/AutoCreateSubtask.php
+++ Action/AutoCreateSubtask.php
@@ -27,7 +27,7 @@ class AutoCreateSubtask extends Base
         return array(
             'column_id' => t('Column'),
             'user_id' => t('Assignee'),
-            'title' => t('Subtask Title'),
+            'title' => t('Subtask Title, leave blank to copy Task Title'),
 	    'time_estimated' => t('Estimated Time in Hours'),                                                                  
             'duration' => t('Duration in days'), 
         );
@@ -40,14 +40,21 @@ class AutoCreateSubtask extends Base
 	    'task' => array(
                 'project_id',
                 'column_id',
+		'title',    
             ),
         );
     }
 
     public function doAction(array $data)
     {
+	 $title_test = $this->getParam('title');
+	 
+	 if (empty ($title_test)) {
+		 $title_test = $this->getParam($data['task']['title'];
+	  }
+	    
 	 $values = array(
-            'title' => $this->getParam('title'),
+            'title' => $title_test,
             'task_id' => $data['task_id'],
             'user_id' => $this->getParam('user_id'),
             'time_estimated' => $this->getParam('time_estimated'),
