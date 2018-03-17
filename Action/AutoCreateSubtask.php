e1d5a0fb3f3e0816104c276307130e9495887857
erwindeclerck <erwin.de.clerck@gmail.com>
2018-03-17 14:26:07 +0100

    Clean up indentation.

--- Action/AutoCreateSubtask.php
+++ Action/AutoCreateSubtask.php
@@ -8,102 +8,101 @@ use Kanboard\Action\Base;
 class AutoCreateSubtask extends Base
 {
 
-    public function getDescription()
-    {
+  public function getDescription()
+  {
     return t('Create one or more Subtasks Automatically');
-    }
+  }
 
-    public function getCompatibleEvents()
-    {
+  public function getCompatibleEvents()
+  {
 
-        return array(
-	    TaskModel::EVENT_CREATE,
-            TaskModel::EVENT_MOVE_COLUMN,
-        );
-    }
+    return array(
+      TaskModel::EVENT_CREATE,
+      TaskModel::EVENT_MOVE_COLUMN,
+    );
+  }
 
-    public function getActionRequiredParameters()
-    {
+  public function getActionRequiredParameters()
+  {
     //changed 'titles' to 'multitasktitles' to have a clean way to render the title-textfield as a textarea
-        return array(
-            'column_id' => t('Column'),
-            'user_id' => t('Assignee'),
+    return array(
+      'column_id' => t('Column'),
+      'user_id' => t('Assignee'),
       'multitasktitles' => t('Subtask Title(s). Leave blank to copy Task Title and create only one subtask'),
-	    'time_estimated' => t('Estimated Time in Hours'),
-            'duration' => t('Duration in days'),
-        );
+      'time_estimated' => t('Estimated Time in Hours'),
+      'duration' => t('Duration in days'),
+    );
+  }
+
+  public function getEventRequiredParameters()
+  {
+    return array(
+      'task_id',
+      'task' => array(
+        'project_id',
+        'column_id',
+        'title',
+      ),
+    );
+  }
+
+  public function doAction(array $data)
+  {
+    //get the value of 'multitasktitles' in stead of the original 'titles'
+    $title_test = $this->getParam('multitasktitles');
+
+    if (empty ($title_test)) {
+      $title_test = $data['task']['title'];
     }
 
-    public function getEventRequiredParameters()
-    {
-        return array(
-            'task_id',
-	    'task' => array(
-                'project_id',
-                'column_id',
-		'title',
-            ),
-        );
-    }
+    $values = array(
+      'title' => $title_test,
+      'task_id' => $data['task_id'],
+      'user_id' => $this->getParam('user_id'),
+      'time_estimated' => $this->getParam('time_estimated'),
+      'time_spent' => 0,
+      'status' => 0,
+      'due_date' => strtotime('+'.$this->getParam('duration').'days'),
+    );
 
-    public function doAction(array $data)
-    {
-   //get the value of 'multitasktitles' in stead of the original 'titles'
-	 $title_test = $this->getParam('multitasktitles');
+    $subtasks = explode("\r\n", isset($values['title']) ? $values['title'] : '');
+    $subtasksAdded = 0;
 
-	 if (empty ($title_test)) {
-		 $title_test = $data['task']['title'];
-	  }
+    foreach ($subtasks as $subtask) {
+      $subtask = trim($subtask);
 
-	 $values = array(
-            'title' => $title_test,
-            'task_id' => $data['task_id'],
-            'user_id' => $this->getParam('user_id'),
-            'time_estimated' => $this->getParam('time_estimated'),
-            'time_spent' => 0,
-            'status' => 0,
-            'due_date' => strtotime('+'.$this->getParam('duration').'days'),
-        );
+      if (! empty($subtask)) {
+        $subtaskValues = $values;
+        $subtaskValues['title'] = $subtask;
 
-        $subtasks = explode("\r\n", isset($values['title']) ? $values['title'] : '');
-        $subtasksAdded = 0;
+        list($valid, $errors) = $this->subtaskValidator->validateCreation($subtaskValues);
 
-        foreach ($subtasks as $subtask) {
-            $subtask = trim($subtask);
-
-            if (! empty($subtask)) {
-                $subtaskValues = $values;
-                $subtaskValues['title'] = $subtask;
-
-                list($valid, $errors) = $this->subtaskValidator->validateCreation($subtaskValues);
-
-                if (! $valid) {
-                    $this->create($values, $errors);
-                    return false;
-                }
-
-                if (! $this->subtaskModel->create($subtaskValues)) {
-                    $this->flash->failure(t('Unable to create your sub-task.'));
-                    $this->response->redirect($this->helper->url->to('TaskViewController', 'show', array('project_id' => $task['project_id'], 'task_id' => $task['id']), 'subtasks'), true);
-                    return false;
-                }
-
-                $subtasksAdded++;
-            }
-        }
-        //restore the messaging with a flash
-        if ($subtasksAdded > 0) {
-          if ($subtasksAdded === 1) {
-            $this->flash->success(t('Subtask added successfully.'));
-          } else {
-            $this->flash->success(t('%d subtasks added successfully.', $subtasksAdded));
-          }
+        if (! $valid) {
+          $this->create($values, $errors);
+          return false;
         }
 
+        if (! $this->subtaskModel->create($subtaskValues)) {
+          $this->flash->failure(t('Unable to create your sub-task.'));
+          $this->response->redirect($this->helper->url->to('TaskViewController', 'show', array('project_id' => $task['project_id'], 'task_id' => $task['id']), 'subtasks'), true);
+          return false;
+        }
+        
+        $subtasksAdded++;
+      }
     }
+    //restore the messaging with a flash
+    if ($subtasksAdded > 0) {
+      if ($subtasksAdded === 1) {
+        $this->flash->success(t('Subtask added successfully.'));
+      } else {
+        $this->flash->success(t('%d subtasks added successfully.', $subtasksAdded));
+      }
+    }
+  }
 
-    public function hasRequiredCondition(array $data)
-    {
-        return $data['task']['column_id'] == $this->getParam('column_id');
-    }
+  public function hasRequiredCondition(array $data)
+  {
+    return $data['task']['column_id'] == $this->getParam('column_id');
+  }
 }
