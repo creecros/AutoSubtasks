16a253245aab28d0c8c398eb4e7103943c70d3b2
manne65-hd <manne@hoffmaenner.net>
2020-03-29 12:32:28 +0200

    Added locale-de_DE

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'Adding automatic actions for subtasks' => 'Fügt automatische Aktionen für Teilaufgaben hinzu',
+    'Create one or more Subtasks Automatically based on column' =>'Erzeuge basierend auf einer Spalte automatisch eine oder mehrere Teilaufgaben',
+    'Create one or more Subtasks Automatically based on a category' => 'Erzeuge basierend auf einer Kategorie automatisch eine oder mehrere Teilaufgaben',
+    'Subtask Title(s)' => 'Titel der Teilaufgabe(n)',
+    'Estimated Time in Hours' => 'Erwartete Zeit in Stunden',
+    'Duration in days' => 'Dauer in Tagen',
+    'Enter one line per task, or leave blank to copy Task Title and create only one subtask.' => 'Geben Sie eine Teilaufgabe pro Zeile ein, oder ...',
+    'Options' => 'Optionen',
+    'Apply to all Columns' => 'Auf alle Spalten anwenden',
+    'Do not duplicate subtasks' => 'Teilaufgaben nicht duplizieren'
+);
